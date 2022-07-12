352e43f97cb430165752b7316be3eb30fc5f8019
Faustino Correa <faustino.correa.m@gmail.com>
2022-07-12 11:17:04 -0400

    descargando una pagina web

--- scraping_la_tercera/main.py
+++ scraping_la_tercera/main.py
@@ -7,4 +7,8 @@ if __name__ == '__main__':
     latercera = requests.get(url)
 
     print(latercera.status_code)
+    print(latercera.headers)
     #print(latercera.text)
+    print(latercera.request.headers)
+    print(latercera.request.method)
+    print(latercera.request.url)
