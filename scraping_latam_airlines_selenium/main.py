6a641878a9bbdca6d7ba1fdfbf741da8f1fd0acd
Faustino Correa <faustino.correa.m@gmail.com>
2022-07-12 20:46:40 -0400

    add Options --InPrivate

--- scraping_latam_airlines_selenium/main.py
+++ scraping_latam_airlines_selenium/main.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.edge.options import Options
+
 
 
 url = 'https://www.latamairlines.com/cl/es'
+PATH = r"C:\Users\Faustino\Documents\GitHub\web_scraping_with_python\scraping_latam_airlines_selenium\_drivers\msedgedriver.exe"
 
 if __name__ == "__main__":
 
+    options = Options()
+    #options.add_argument("headless")
+    options.add_argument('--InPrivate')
 
-    driver = webdriver.Edge(executable_path='.\_drivers\msedgedriver.exe')
 
-    driver.get(url)
+    #driver = webdriver.Edge(executable_path=PATH)
+    driver = webdriver.Edge(executable_path=PATH, options = options)
 
     driver.close()
